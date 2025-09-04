b810d6bf9bca60aa1fd8f70e9657b1d977dccc56
st1abps-star <st1.abps@gmail.com>
2025-09-04 14:03:05 +0530

    Updated PortfolioSection.jsx

--- src/components/home/PortfolioSection.jsx
+++ src/components/home/PortfolioSection.jsx
@@ -52,7 +52,7 @@ const PortfolioSection = () => {
     gsap.to(trackRef.current, {
       xPercent: -50,
       repeat: -1,
-      duration: 10,
+      duration: 25,
       ease: "linear"
     })
   }, [])
